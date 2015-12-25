0cca9379c34bf210acf289efa7834618b1b2ad25
Adrian Bukowski <adrianbukowski93@gmail.com>
2015-12-25 14:22:47 +0100

    93 pages behind

---
+++ src/over_load_ride/Override1.java
@@ -0,0 +1,43 @@
+package over_load_ride;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Override1 {
+	void test() throws Exception {
+	}
+
+	static void OverrideMe() {// Can't override static method !
+	}
+
+}
+
+class Child extends Override1 {
+	void test() throws IOException, SQLException { // both are subtypes of
+		// Exception exception.
+		try { // Had to use try catch block because of
+			// unhandled Exception exception
+			super.test(); // Calling overridden method.
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	static void OverrideMe() {// It's a new method. We didn't override it! We
+								// can't use super too.
+
+	}
+}
+
+// TITBIT : Overriding method must be compatible in terms of : list of
+// arguments,access modifiers,
+// declared exceptions and returned type.
+// #1 - specificator can be the same r less restricting.
+// #2 - argument's types must be the same,
+// #3 - returned type must be the same or a subtype of this type.
+// #4 - exceptions - overriding method can't declare new CHECKED
+// exceptions(trial will result in compilation error),
+// it can declare smaller amount of them or exceptions which are subtypes of
+// thosedeclared in overridden method.
+// What's more this method can declare as many unchecked exc's as you wish.
--- src/package2/Test2.java
+++ src/package2/Test2.java
@@ -16,26 +16,33 @@ strictfp class Class1 { // Class widoczna tylko w pakiecie
 
 	}
 
-	private strictfp int method1() {// Name of the method should be a verb + noun, e.g. getData(). 
+	private strictfp int method1() {// Name of the method should be a verb +
+									// noun, e.g. getData().
 		return 1;
 	}
 
 	native private int method2(); // native always at the beginning. Method is
 									// without body! It's used just for methods.
 	// can't be native and strictfp
+
+	public static final synchronized native int method21();
+	// longest possible sereies of specifiers.
+
 }
 
 abstract strictfp class Class2 { // "class abstract" will make an error. Order
 									// matters!
-								// We can't create any instance of this class. 
+	// We can't create any instance of this class.
 	abstract float method3();
 
 	abstract char mehod4();
 
-	int method4() { // there can be non abstract method in abstract class
+	int method4() { // there can be  non abstract method in abstract class
 		return -1;
 	}
-
+	
+	// static abstract int meth1(); Not allowed ! abstract method can be only
+	// public or protected, not even static,strictfp etc!
 }
 
 abstract strictfp interface Inter { // Interface can be abstract but
@@ -45,7 +52,6 @@ abstract strictfp interface Inter { // Interface can be abstract but
 
 public class Test2 implements Interface1 {
 
-
 	public int methodInt() {// When implementing a method from interface it must
 							// be public!
 		return 2;
@@ -53,9 +59,9 @@ public class Test2 implements Interface1 {
 
 	public Double methodNumber() { // Signature doesn't have to be the same as
 									// in interface.
-									// Types must be just acceptable and exceptions are not obligatory.
+									// Types must be just acceptable and
+									// exceptions are not obligatory.
 		return 2.0;
 	}
-	
 
 }
